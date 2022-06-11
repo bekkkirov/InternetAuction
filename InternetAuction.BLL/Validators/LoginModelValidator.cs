699e4567a27bc0a41ad78f83448027dfe51ca11c
bekirov <vbekirovv@gmail.com>
2022-06-11 15:09:45 +0300

    Added fluent validation for authorization models.

---
+++ InternetAuction.BLL/Validators/LoginModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using InternetAuction.BLL.Models;
+
+namespace InternetAuction.BLL.Validators
+{
+    public class LoginModelValidator : AbstractValidator<LoginModel>
+    {
+        public LoginModelValidator()
+        {
+            RuleFor(l => l.UserName)
+                .NotEmpty();
+
+            RuleFor(l => l.Password)
+                .NotEmpty();
+        }
+    }
+}
---
+++ InternetAuction.BLL/Validators/RegisterModelValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using InternetAuction.BLL.Models;
+
+namespace InternetAuction.BLL.Validators
+{
+    public class RegisterModelValidator : AbstractValidator<RegisterModel>
+    {
+        public RegisterModelValidator()
+        {
+            RuleFor(r => r.UserName)
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(20);
+
+            RuleFor(r => r.FirstName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(30);
+            
+            RuleFor(r => r.FirstName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(30);
+
+            RuleFor(r => r.Email)
+                .EmailAddress();
+        }
+    }
+}
