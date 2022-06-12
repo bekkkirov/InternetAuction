adc81278a316bc0992eb4352acba28bb1f91eeae
bekirov <vbekirovv@gmail.com>
2022-06-12 22:51:04 +0300

    Added nav-bar component.

--- InternetAuction.API/Client/src/app/app.component.ts
+++ InternetAuction.API/Client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from "./core/services/account.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit{
+    title = 'Client';
+
+    constructor(private accountService: AccountService) {
+    }
+
+    ngOnInit(): void {
+        let currentUser = JSON.parse(localStorage.getItem('user'));
+        console.log(currentUser);
+        this.accountService.setCurrentUser(currentUser);
+    }
 }
--- InternetAuction.API/Client/src/app/app.module.ts
+++ InternetAuction.API/Client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import {SignInComponent} from "./core/components/sign-in/sign-in.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import { SignUpComponent } from './core/components/sign-up/sign-up.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         SignInComponent,
         SignUpComponent,
+        NavBarComponent,
     ],
     imports: [
         BrowserModule,
