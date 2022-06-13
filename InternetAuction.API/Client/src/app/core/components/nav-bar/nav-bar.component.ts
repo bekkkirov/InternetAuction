65f6599dd0bc23a8b3c4c0b8e5c19f8ea982fada
bekirov <vbekirovv@gmail.com>
2022-06-13 17:40:23 +0300

    Added profile dropdown to navbar.

--- InternetAuction.API/Client/src/app/core/components/nav-bar/nav-bar.component.ts
+++ InternetAuction.API/Client/src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AccountService} from "../../services/account.service";
+import {LoggedInUserModel} from "../../models/LoggedInUserModel";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+    constructor(public accountService: AccountService) {
+    }
 
-  constructor(public accountService: AccountService) { }
+    ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
+    }
 
-  logout() {
+    logout() {
 
-  }
+    }
 }
