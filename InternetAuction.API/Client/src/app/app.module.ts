c739ca9137aff18f9b9af7a325800a2a6e66f4c6
bekirov <vbekirovv@gmail.com>
2022-06-13 12:35:44 +0300

    Added error interceptor, toaster module.

--- InternetAuction.API/Client/src/app/app.module.ts
+++ InternetAuction.API/Client/src/app/app.module.ts
@@ -5,12 +5,15 @@ import {AppComponent} from './app.component';
 import {SignInComponent} from "./core/components/sign-in/sign-in.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import { SignUpComponent } from './core/components/sign-up/sign-up.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
 import { LotsListComponent } from './core/components/lots-list/lots-list.component';
 import { UserDetailComponent } from './core/components/user-detail/user-detail.component';
 import {AppRoutingModule} from "./core/app-routing/app-routing.module";
 import { UserEditComponent } from './core/components/nav-bar/user-edit/user-edit.component';
+import {ToastrModule} from "ngx-toastr";
+import {ErrorInterceptor} from "./core/interceptors/error.interceptor";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
     declarations: [
@@ -24,11 +27,15 @@ import { UserEditComponent } from './core/components/nav-bar/user-edit/user-edit
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
+        ToastrModule.forRoot({positionClass: 'toast-bottom-right'})
     ],
-    providers: [],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
