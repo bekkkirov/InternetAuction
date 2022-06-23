de7719ff5db9dc70e0ef798dab640a1a160a1a94
bekirov <vbekirovv@gmail.com>
2022-06-23 16:19:07 +0300

    Implemented user profile. Updated nav bar.

--- InternetAuction.API/Client/src/app/core/components/user-detail/user-detail.component.ts
+++ InternetAuction.API/Client/src/app/core/components/user-detail/user-detail.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserModel} from "../../models/user.model";
+import {UserService} from "../../services/user.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+    selector: 'app-user-detail',
+    templateUrl: './user-detail.component.html',
+    styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+    user: UserModel;
+    pageNumber: number = 1;
+    tab: string = "regLots"
 
-  constructor() { }
+    constructor(private userService: UserService, private route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.userService.getByUserName(this.route.snapshot.paramMap.get('userName'))
+                        .subscribe(result => this.user = result);
+    }
 
+    changePage(event) {
+        this.pageNumber = event;
+    }
+
+    changeTab(tab: string) {
+        this.tab = tab;
+    }
 }
--- InternetAuction.API/Client/src/app/core/models/user.model.ts
+++ InternetAuction.API/Client/src/app/core/models/user.model.ts
@@ -1,14 +1,15 @@
 import {LotModel} from "./lot.model";
 import {BidModel} from "./bid.model";
+import {ImageModel} from "./image.model";
 
 export interface UserModel {
     id: number,
     userName: string,
     firstName: string,
     lastName: string,
-    balance: string
-    profileImageId: number
-    registeredLots: LotModel[]
-    boughtLots: LotModel[]
+    balance: string,
+    profileImage: ImageModel,
+    registeredLots: LotModel[],
+    boughtLots: LotModel[],
     bids: BidModel[]
 }
